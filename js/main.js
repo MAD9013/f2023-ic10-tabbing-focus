b1bfd2f9de1309d3c7adae67e9614ce0c0e502c6
Adam Robillard <hello@adamrobillard.ca>
2023-11-11 11:46:37 -0500

    Update for F2023

--- js/main.js
+++ js/main.js
@@ -1,19 +1,37 @@
 function init() {
-  
-  const toTop = document.querySelector('.to-top');
+  const toggles = document.querySelectorAll(".toggle");
+
+  function handleToggle(toggle) {
+    const targetId = toggle.dataset.target;
+
+    const target = document.getElementById(targetId);
+    const clickToClose = document.querySelector(`[data-toggle="${targetId}"]`);
+
+    function updateState() {
+      toggle.classList.toggle("open");
+      target.classList.toggle("open");
+      clickToClose.classList.toggle("open");
+    }
+
+    toggle.addEventListener("click", updateState);
+    clickToClose.addEventListener("click", updateState);
+  }
+
+  toggles.forEach((toggle) => handleToggle(toggle));
+
+  const toTop = document.querySelector(".to-top");
 
   function handleToTop() {
     if (window.scrollY > 50) {
-      toTop.classList.add('visible');
+      toTop.classList.add("visible");
     } else {
-      toTop.classList.remove('visible');
+      toTop.classList.remove("visible");
     }
   }
 
   toTop && handleToTop();
 
-  toTop && window.addEventListener('scroll', handleToTop);
-
+  toTop && window.addEventListener("scroll", handleToTop);
 }
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
